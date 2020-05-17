0c174c6299239a5f0dbbab36b2949a6789866863
Alessandre Martins <sandremartins@gmail.com>
2020-05-17 16:24:29 -0400

    d

---
+++ build/precache-manifest.3eada95eb6b72ae200ef7dd3a5ff005c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f474e7d70dd1b4a6cdb706e0efd65a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8475def79c4d4855960",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "f8475def79c4d4855960",
+    "url": "/static/js/2.e25187e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e25187e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cc38814be9b5d31596",
+    "url": "/static/js/main.92cd17c3.chunk.js"
+  },
+  {
+    "revision": "65d8f961245b286f01c9",
+    "url": "/static/js/runtime-main.8bafaf31.js"
+  }
+]);
